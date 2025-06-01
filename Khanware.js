c138e86a093aabff777ff6fcbb46ed6c5f59758c
sheeshmainlol <109035035+sheeshmainlol@users.noreply.github.com>
2025-06-01 15:15:55 -0300

    Update Khanware.js

--- Khanware.js
+++ Khanware.js
@@ -1,5 +1,5 @@
 const ver = "V3.1.5";
-let isDev = true;
+let isDev = false;
 
 const repoPath = `https://raw.githubusercontent.com/sheeshmainlol/random-things/refs/heads/${isDev ? "dev/" : "main/"}`;
 
